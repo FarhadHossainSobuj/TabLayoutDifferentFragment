b99233cbd691e45e32d42e1eb756fd266e0ecdab
Farhad Hossain <tlcfarhad@gmail.com>
2020-05-13 18:13:12 +0600

    Initial commit

---
+++ app/src/main/java/com/example/tablayoutdifferentfragment/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,65 @@
+package com.example.tablayoutdifferentfragment.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.tablayoutdifferentfragment.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+
+        switch (position){
+            case 0:
+                fragment = new Frag1();
+                break;
+            case 1:
+                fragment = new Frag2();
+                break;
+            case 2:
+                fragment = new Frag3();
+                break;
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Fragment 1";
+            case 1:
+                return "Fragment 2";
+            case 2:
+                return "Fragment 3";
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}
